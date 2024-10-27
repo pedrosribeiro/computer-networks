266d36a8fd84bffb5f9824f7cb96783f866e98c3
Pedro Scarpin Ribeiro <pedroscarpinribeiro@gmail.com>
2024-10-27 16:52:05 -0300

    feat: sending checksum before block of data

--- udp/server.py
+++ udp/server.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 import socket
 
@@ -7,41 +8,58 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(UDP_ADDR)
 
 
+def calculate_checksum(data: bytes) -> str:
+    return hashlib.md5(data).hexdigest()
+
+
 def send_file(filename: str, addr: tuple, encoding: str = ENCODING) -> None:
-    begin_msg = "BEGIN 200".encode(encoding=encoding)
-    sock.sendto(begin_msg, addr)
+    begin_msg = "BEGIN 200"
+    send_message(begin_msg, addr, False, encoding)
 
     with open(filename, mode="rb") as file:
         while True:
             data = file.read(BUF_SIZE)
             if not data:
                 break
-            sock.sendto(data, addr)
 
-    sock.sendto(END_BYTE, addr)
+            checksum = calculate_checksum(data)
+            send_message(checksum, addr, False, encoding)
+
+            send_message(data, addr, False, encoding)
+
+    send_message(END_BYTE, addr, False, encoding)
     print(f"File {filename} sent to client. Transfer completed")
 
 
-def send_message(msg: str, addr: tuple, encoding: str = ENCODING) -> None:
-    msg_enc = msg.encode(encoding=encoding)
+def send_message(
+    msg: str | bytes,
+    addr: tuple,
+    end_byte: bool = True,
+    encoding: str = ENCODING,
+) -> None:
+    if isinstance(msg, str):
+        msg_enc = msg.encode(encoding=encoding)
+    else:
+        msg_enc = msg
     msg_enc_len = len(msg_enc)
 
     for i in range(0, msg_enc_len, BUF_SIZE):
         sock.sendto(msg_enc[i : i + BUF_SIZE], addr)
 
-    sock.sendto(END_BYTE, addr)
+    if end_byte:
+        sock.sendto(END_BYTE, addr)
 
     print(f"Message sent to client: {msg}")
 
 
-def handle_message(encoded_msg: bytes, addr) -> None:
+def handle_message(encoded_msg: bytes, addr: tuple, encoding: str = ENCODING) -> None:
     decoded_msg = encoded_msg.decode(ENCODING)
     if decoded_msg.startswith("GET /"):
         filename = decoded_msg.split("/")[1]
         if os.path.isfile(filename):
-            send_file(filename, addr)
+            send_file(filename, addr, encoding)
         else:
-            send_message("BEGIN 404", addr)
+            send_message("BEGIN 404", addr, True, encoding)
     else:
         print("Protocol not recognized! Message discarded.")
 
